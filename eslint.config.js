8e0cc5c4c1610636ec198b99c3e97f2b3d06ab83
SeHyun <rla6170@naver.com>
2025-07-29 15:53:43 +0900

    chore: eslint-import-resolver-typescript 설치 및 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,46 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import unusedImports from "eslint-plugin-unused-imports";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import importPlugin from "eslint-plugin-import";
-import reactPlugin from "eslint-plugin-react";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import unusedImports from 'eslint-plugin-unused-imports';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
       eslintConfigPrettier, // prettier eslint 호환성 플러그인
       importPlugin.flatConfigs.recommended, // import export 관련 플러그인
-      reactPlugin.configs.flat.recommended, // react 관련 플러그인 
-      reactPlugin.configs.flat['jsx-runtime'] // React 17 +
+      reactPlugin.configs.flat.recommended, // react 관련 플러그인
+      reactPlugin.configs.flat['jsx-runtime'], // React 17 +
     ],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       unicorn: eslintPluginUnicorn, // 현대적인 문법 지원
-      "unused-imports": unusedImports, // 미사용 import 제거
+      'unused-imports': unusedImports, // 미사용 import 제거
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: path.resolve('./tsconfig.json'), // 절대 경로 설정 오류
+        },
+      },
     },
   }
 );
