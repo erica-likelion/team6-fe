5dd4ad3f302c1cdfb3b042527c3b898749b358b4
SeHyun <rla6170@naver.com>
2025-08-01 14:27:43 +0900

    chore: vite 기반 project tailwind 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    tailwindcss(),
     VitePWA({
       registerType: 'prompt',
       injectRegister: false,
