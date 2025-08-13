47af8d6ac340449c19336d9515016bc6aff1a65a
SeHyun <rla6170@naver.com>
2025-08-13 11:28:14 +0900

    fix: main component min height

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,7 +4,7 @@ import type { LayoutProps } from './type';
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="bg-primary-bg flex h-[100%] flex-col justify-between">
-      <main className="flex-1">{children}</main>
+      <main className="flex min-h-0 flex-1 flex-col">{children}</main>
       <Nav />
     </div>
   );
