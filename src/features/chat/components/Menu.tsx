03f864d46e815b1a1c4e12aa60d198e60453b1a8
SeHyun <rla6170@naver.com>
2025-08-18 18:37:12 +0900

    feat: 채팅방 input 가상 키보드 반응형 UI - safari only

---
+++ src/features/chat/components/Menu.tsx
@@ -0,0 +1,45 @@
+import { Icon } from '@components/Icon';
+import type { MenuType } from '@features/chat/types/menu.type';
+import { useId } from 'react';
+
+const MENUS: MenuType = [
+  {
+    icon: 'camera_fill',
+    label: '카메라',
+  },
+  {
+    icon: 'image_fill',
+    label: '사진',
+  },
+  {
+    icon: 'clock-plus_fill',
+    label: '약속 잡기',
+  },
+  {
+    icon: 'paper-fold-text_fill',
+    label: '정산하기',
+  },
+  {
+    icon: 'door-exit_fill',
+    label: '나가기',
+  },
+];
+
+export const Menu = () => {
+  const menuId = useId();
+  return (
+    <ul className="flex h-[15rem] w-full flex-wrap content-center justify-center gap-x-[0rem] gap-y-[1.12rem]">
+      {MENUS.map((menu, index) => (
+        <li
+          key={menuId + index}
+          className="flex h-[4.25rem] basis-1/3 flex-col items-center justify-center gap-[0.5rem]"
+        >
+          <div className="flex h-[2.75rem] w-[2.75rem] items-center justify-center rounded-[1.375rem] bg-white">
+            <Icon icon={menu.icon} width={'1.5rem'} height={'1.5rem'} className="text-gray-500" />
+          </div>
+          <span className="label-small-500 text-gray-500">{menu.label}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/features/chat/components/Room.tsx
@@ -0,0 +1,36 @@
+import { Input } from '@features/chat/components/Input';
+import { Header } from '@features/chat/components/Header';
+import { Messages } from '@features/chat/components/Messages';
+import { useRef, useEffect, useState } from 'react';
+
+interface RoomProps {
+  title: string;
+  memberCount: number;
+}
+
+export const Room = ({ title, memberCount }: RoomProps) => {
+  const [isKeyboard, setIsKeyboard] = useState(false);
+  const [kb, setKb] = useState(0);
+  const initialHeight = useRef(visualViewport?.height);
+
+  useEffect(() => {
+    const handleVisualViewportResize = () => {
+      const currentVisualViewport = Number(window.visualViewport?.height);
+      if (currentVisualViewport != initialHeight.current && initialHeight.current !== undefined) {
+        window.scrollTo(0, 0); // 가상 키보드 활성화 시 화면 밀림 방지
+        setIsKeyboard(true); // 가상 키보드 활성화 여부 판단
+        setKb(initialHeight.current - currentVisualViewport); // 가상 키보드 높이 판별
+      }
+    };
+    visualViewport?.addEventListener('resize', handleVisualViewportResize); // 가상키보드 함수 구독
+    return () => visualViewport?.removeEventListener('resize', handleVisualViewportResize); // 언마운트 시 구독 제거
+  }, []);
+
+  return (
+    <div className="bg-primary-bg absolute z-11 h-dvh w-dvw">
+      <Header title={title} memberCount={memberCount} />
+      <Messages />
+      <Input isKeyboard={isKeyboard} setIsKeyboard={setIsKeyboard} bottom={kb} />
+    </div>
+  );
+};
