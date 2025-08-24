94891f9c3faf8c22f5fc0ff9694bdf2e048c5d55
SeHyun <rla6170@naver.com>
2025-08-24 21:24:29 +0900

    refactor: header 공통 컴포넌트 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,13 +11,17 @@ export const Header = ({ title, memberCount, isMenu }: HeaderProps) => {
   const route = useRouter();
   return (
     <header className="bg-primary-bg fixed top-0 flex h-[3.25rem] w-full items-center justify-between px-[1rem]">
-      <Icon icon="chevron" onClick={() => route.history.back()} />
+      <div className="flex h-[2.75rem] w-[2.75rem] items-center justify-center">
+        <Icon icon="chevron" onClick={() => route.history.back()} />
+      </div>
       {/** 뒤로가기 */}
       <div className="flex items-center gap-[0.5rem]">
         <span className="label-medium font-bold">{title}</span>
         {memberCount && <span className="label-xsmall text-gray-500">{memberCount}</span>}
       </div>
-      <div>{isMenu && <Icon icon="more-menu" className="rotate-90" />}</div>
+      <div className="flex h-[2.75rem] w-[2.75rem] items-center justify-center">
+        {isMenu && <Icon icon="more-menu" className="rotate-90" />}
+      </div>
       {/** 게시물 보러가기, 나가기, 신고하기 게시물 id, chat id 관련 작업 */}
     </header>
   );
