77dde75eb0b9c022e9a63df1a0a809982ff3e930
SeHyun <rla6170@naver.com>
2025-08-25 10:24:06 +0900

    feat: bottom nav z-index 반영

--- src/features/post/components/Post.tsx
+++ src/features/post/components/Post.tsx
@@ -1,7 +1,7 @@
 // features/post/components/Post.tsx
 import { Header } from '@components/Header/Header';
 import { FormProvider, useForm } from 'react-hook-form';
-import { useSearch } from '@tanstack/react-router';
+import { useNavigate, useSearch } from '@tanstack/react-router';
 
 import { PartyBoard } from '@features/post/components/PartyBoard';
 import { ItemBoard } from '@features/post/components/ItemBoard';
@@ -39,7 +39,7 @@ export const Post = () => {
   // /post?type=party | /post?type=item
   const { type } = useSearch({ from: '/post/write/' }) as { type?: 'party' | 'item' };
   const isParty = (type ?? 'party') === 'party';
-
+  const navigate = useNavigate();
   const methods = useForm<FormValues>({
     mode: 'onChange',
     defaultValues: (isParty
@@ -69,8 +69,6 @@ export const Post = () => {
   const {
     handleSubmit,
     formState: { isSubmitting, isValid },
-    reset,
-    getValues,
   } = methods;
 
   const onSubmit = async (values: FormValues) => {
@@ -113,16 +111,7 @@ export const Post = () => {
         } satisfies CreateItemInput);
       }
 
-      alert('게시글이 등록되었습니다!');
-      reset({
-        ...getValues(),
-        title: '',
-        content: '',
-        photos: undefined,
-        ...(values.type === 'party' ? { eventTime: '', itemCodes: [] } : { price: 0, receipts: undefined }),
-        agreement1: false,
-        agreement2: false,
-      } as FormValues);
+      navigate({ to: '/home' });
     } catch (e) {
       const message = e instanceof Error ? e.message : typeof e === 'string' ? e : '등록 중 오류가 발생했습니다.';
       alert(message);
@@ -130,7 +119,7 @@ export const Post = () => {
   };
 
   return (
-    <div className="bg-primary-bg relative flex min-h-dvh w-dvw flex-col">
+    <div className="bg-primary-bg absolute z-51 flex h-dvh w-dvw flex-col overflow-y-scroll">
       <div className="bg-primary-bg sticky top-0 z-20">
         <Header title={`게시글 작성하기`} isMenu={false} />
       </div>
